f0e87744928057778b859ed5cfaf58f6cf2d8ab0
laoriy <2680406465@qq.com>
2021-07-20 19:18:01 +0800

    feat: remove code of auto install

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,4 @@ const install = (app: App) => {
     app.component('JsonViewer', JsonView);
 };
 
-// auto install
-if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue);
-}
 export default Object.assign(JsonView, { install });
